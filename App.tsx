fc8e53a84e3a02a26ed7e2d554b1875354a972bf
iqbalbinothman-ctrl <iqbalbinothman@gmail.com>
2026-02-02 10:33:14 +0800

    Exclude Shoji page from maintenance mode completely

--- App.tsx
+++ App.tsx
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import ShojiPage from './components/ShojiPage';
 import MaintenanceMode from './components/MaintenanceMode';
+
+const AppContent: React.FC<{ isMaintenanceMode: boolean }> = ({ isMaintenanceMode }) => {
+  const location = useLocation();
+  const isHomepage = location.pathname === '/';
+
+  return (
+    <div className="relative">
+      {/* Apply blur only on homepage when maintenance mode is active */}
+      <div className={isMaintenanceMode && isHomepage ? 'blur-sm pointer-events-none' : ''}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shojiMP_2026" element={<ShojiPage />} />
+        </Routes>
+      </div>
+
+      {/* Maintenance Mode Overlay (only on homepage) */}
+      {isMaintenanceMode && isHomepage && <MaintenanceMode />}
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   const [isMaintenanceMode] = useState(true); // Set to false to disable
 
   return (
     <Router>
-      <div className="relative">
-        {/* Homepage with blur when maintenance mode is active */}
-        <div className={isMaintenanceMode ? 'blur-sm pointer-events-none' : ''}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shojiMP_2026" element={<ShojiPage />} />
-          </Routes>
-        </div>
-
-        {/* Maintenance Mode Overlay (only on homepage) */}
-        {isMaintenanceMode && window.location.pathname === '/' && <MaintenanceMode />}
-      </div>
+      <AppContent isMaintenanceMode={isMaintenanceMode} />
     </Router>
   );
 };
